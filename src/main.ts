7e175ffc0ebcc8a1d283e8a0115cac08afd03eeb
Bruno Silva <brunosilvabrait@gmail.com>
2025-07-26 14:20:54 -0300

    docs(swagger): add Swagger documentation setup with YAML file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { load } from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,11 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerDocument: OpenAPIObject = load(
+    './swagger.yaml',
+  ) as OpenAPIObject;
+  SwaggerModule.setup('api', app, swaggerDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
