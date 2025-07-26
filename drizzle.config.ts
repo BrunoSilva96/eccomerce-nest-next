47e942712764005249fa4fa8af7f99109ae61a25
Bruno Silva <brunosilvabrait@gmail.com>
2025-07-26 13:49:07 -0300

    chore(drizzle): load environment variables using dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
-  schema: './src/database/schema',
+  schema: './src/database/schemas',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
